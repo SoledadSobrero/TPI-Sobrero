ea108f2dbb85923558767555ebfc4308295ae084
SoledadSobrero <solesobrero.92@gmail.com>
2025-07-24 20:53:40 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import MessagesContextProvider from './Context/MessageContext.jsx'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 import ContactContextProvider from './Context/ContactContex.jsx'
 
 createRoot(document.getElementById('root')).render(
